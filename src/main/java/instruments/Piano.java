0326c1cd8a69559d99cdccb3fa03f786966301c8
Jim Bevington <jim.bevington@hotmail.com>
2018-03-09 12:24:29 +0000

    created Piano class, PianoType enum and piano test

--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,8 +1,24 @@
-//package instruments;
-//
-//import enums.InstrumentType;
-//
-//public class Piano {
-//    public Piano(String name, String manufacturer, int quantity, double buyPrice, double sellPrice, InstrumentType keyboard, String black, String balsawood, Object p8, Object p9, int i1) {
-//    }
-//}
+package instruments;
+
+import enums.InstrumentType;
+import enums.PianoType;
+import stock.Instrument;
+
+public class Piano extends Instrument {
+
+    private PianoType type;
+    private int noOfKeys;
+
+    public Piano(String name, String manufacturer, int quantity, double buyPrice, double sellPrice, String colour,
+                 String material, PianoType type, int noOfKeys) {
+        super(name, manufacturer, quantity, buyPrice, sellPrice, InstrumentType.KEYBOARD, colour, material);
+        this.type = type;
+        this.noOfKeys = noOfKeys;
+    }
+
+
+    @Override
+    public String play() {
+        return "Plink, plonk, crunch, smash!";
+    }
+}
