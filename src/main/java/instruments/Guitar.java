9745ef66898cba76da905f7935977836e426af41
Jim Bevington <jim.bevington@hotmail.com>
2018-03-09 11:57:51 +0000

    added GuitarType enum

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -1,14 +1,22 @@
 package instruments;
 
 
+import enums.GuitarType;
 import enums.InstrumentType;
 import stock.Instrument;
 
 public class Guitar extends Instrument {
 
 
-    public Guitar(String name, String manufacturer, int quantity, double buyPrice, double sellPrice, InstrumentType instrumentType, String colour, String material) {
+    private GuitarType guitarType;
+    private int stringNumber;
+
+    public Guitar(String name, String manufacturer, int quantity, double buyPrice, double sellPrice,
+                  InstrumentType instrumentType, String colour, String material, GuitarType guitarType,
+                  int stringNumber) {
         super(name, manufacturer, quantity, buyPrice, sellPrice, instrumentType, colour, material);
+        this.guitarType = guitarType;
+        this.stringNumber = stringNumber;
     }
 
     @Override
